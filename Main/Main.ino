567984b0fd9771d2dce36eb24428bed5754193b4
Thomas Cibils <thomas.cibils@gmail.com>
2020-06-06 22:06:45 +0200

    Deleting extra line
    Now complies with purely arbitrary code standards x)

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,5 +1,4 @@
 /* 
- *  
  *  Creator : Thomas Cibils & Matteo Raviglione
  *  FastLED tuto : https://github.com/FastLED/FastLED/wiki/Basic-usage - used for WS2812B 5050 RGB LED Strip 5M 150 300 Leds 144 60LED/M Individual Addressable 5V
  *  */
